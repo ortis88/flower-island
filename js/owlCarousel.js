d92ffd6b072202c4caf05119cf3f18e73e98956e
ortis88 <caesar0718@gmail.com>
2024-08-08 14:57:04 +0800

    init

---
+++ js/owlCarousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items: 2,
+      center:true,
+      loop:true,
+      margin: 10,
+      dots:false,
+      autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    });
+  });
